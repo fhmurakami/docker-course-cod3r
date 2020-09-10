f6f57c836270a29f7b4e19b63a08a297e6ceddf0
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-09-10 09:56:34 -0300

    Add networks to docker compose

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -1,9 +1,25 @@
+import psycopg2
 from bottle import route, run, request
+
+DSN = f'dbname=email_sender user=postgres host=db password=postgres'
+SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+
+def register_message(subject, message):
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    cur.execute(SQL, (subject, message))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Message registered!')
 
 @route('/', method='POST')
 def send():
     subject = request.forms.get('subject')
     message = request.forms.get('message')
+
+    register_message(subject, message)
     return 'Message enqueued! Subject: {} Message: {}'.format(
       subject, message
     )
