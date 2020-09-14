f136385ed26ca1bae879f02963bdf1ad8e8b28f4
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-09-14 13:21:33 -0300

    Add environment variables to docker compose

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -1,6 +1,7 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 
@@ -8,9 +9,15 @@ class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.queue= redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = f'dbname=email_sender user=postgres host=db password=postgres'
-        self.conn = psycopg2.connect(DSN)
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        self.queue= redis.StrictRedis(host=redis_host, port=6379, db=0)
+
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_name = os.getenv('DB_NAME', 'sender')
+        db_password = os.getenv('DB_PASSWORD', 'postgres')
+        dsn = f'dbname={db_name} user={db_user} host={db_host} password={db_password}'
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, subject, message):
         SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
